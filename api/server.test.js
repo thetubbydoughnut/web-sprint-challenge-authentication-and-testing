41d34d84df8c83eb12998d6c97a6df26a52a11fe
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-11-03 12:37:32 -0600

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,27 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const request = require('supertest');
+const server = require('./server');
+const db = require('../data/dbConfig');
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe('auth endpoints', () => {
+  describe('[POST] /api/auth/register', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+    });
+
+    test('responds with 400 if username or password missing', async () => {
+      const res = await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'test' });
+      expect(res.body).toBe("username and password required");
+    });
+  });
+});    
